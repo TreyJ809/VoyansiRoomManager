002e242d9617a1b07015278a86f1e4dde8331f2a
TreyJ <treyj809@yahoo.com>
2022-10-13 22:26:30 -0400

    Adding Maven wrapper and Dockerizing

--- src/main/java/com/sample/app/App.java
+++ src/main/java/com/sample/app/App.java
@@ -2,6 +2,7 @@ package com.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +16,10 @@ public class App
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
     }
 }
