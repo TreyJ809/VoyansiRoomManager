74f3215fb1f8bb61d874f2b3a7a091070beb8fad
TreyJ <treyj809@yahoo.com>
2022-10-13 23:50:44 -0400

    Stub for Room data

---
+++ src/main/java/com/sample/app/Room.java
@@ -0,0 +1,35 @@
+package com.sample.app;
+
+public class Room {
+	
+	private String id;
+	private String name;
+	private int number;
+	private String occupant;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getOccupant() {
+		return occupant;
+	}
+	public void setOccupant(String occupant) {
+		this.occupant = occupant;
+	}
+
+}
