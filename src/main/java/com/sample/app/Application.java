4a4c71d12c01d6ed6c245fb8904ade26443cf66a
TreyJ <treyj809@yahoo.com>
2022-10-16 21:07:02 -0400

    Catching up with missed change

--- src/main/java/com/sample/app/Application.java
+++ src/main/java/com/sample/app/Application.java
@@ -44,6 +44,7 @@ public class Application
 		AmazonDynamoDB client = createDynamoDBClient();
 		DynamoDB ddb = new DynamoDB(client);
 		Table roomTable = ddb.getTable("room");
+		roomTable.scan()
 		
 //		roomTable.
 		
